8ce36a65a8e645cbb45fd991d80e5efbfb2af1cf
Dhsking <ulrich.dahissiho@epitech.eu>
2024-05-07 14:36:05 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {MainMenu} from "./structure/MainMenu"
+import { MenuButton } from "./component/MyButton"
+
+function App() {
+
+  return (
+    <div>
+      <MainMenu />
+    </div>
+  )
+}
+
+export default App
